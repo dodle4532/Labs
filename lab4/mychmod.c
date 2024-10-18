2c38b9cf44bf16b83e58900a1c7a72554f14fbc9
dodle4532 <120771908+dodle4532@users.noreply.github.com>
2024-10-18 11:51:03 +0300

    Update mychmod.c

--- lab4/mychmod.c
+++ lab4/mychmod.c
@@ -7,7 +7,19 @@
 #include <math.h>
 
 bool isPlus = false;
+char* path;
 
+/*
+S_IRUSR: Owner Read permission (Owner Read)
+S_IWUSR: Owner Write permission (Owner Write)
+S_IXUSR: Owner Execute permission (Owner Execute)
+S_IRGRP: Group member read permission (Group Read)
+S_IWGRP: Group member write permission (Group Write)
+S_IXGRP: Execute permission for group members (Group Execute)
+S_IROTH: Read permission for other users (Others Read)
+S_IWOTH: Write permission for other users (Others Write)
+S_IXOTH: Execute permission for other users (Others Execute)
+*/
 
 mode_t getMode(char first, char second) {
     if (first == 'u' && second == 'r') {
@@ -40,6 +52,19 @@ mode_t getMode(char first, char second) {
     return 0;
 }
 
+void doChmod(char* fileName, mode_t* modes) {
+    int i = 1;
+    mode_t final_mode = modes[0];
+    while (modes[i] != 0) {
+        final_mode |= modes[i];
+        i++;
+    }
+    struct stat file_stat;
+    stat(fileName, &file_stat);
+    if (isPlus) chmod(fileName, file_stat.st_mode | final_mode);
+    else chmod(fileName, file_stat.st_mode & ~final_mode);
+}
+
 mode_t* getPermisson(char* string) {
     mode_t* modes = (mode_t*) calloc(16, sizeof(mode_t));
     int index = 0;
@@ -48,6 +73,18 @@ mode_t* getPermisson(char* string) {
     bool isFirst = true;
     int j = 0;
     for (int i = 0; i < strlen(string); ++i) {
+        if (string[i] == '=') {
+            char* str = (char*)calloc(16, sizeof(char));
+            strcpy(str, "-rwx");
+            mode_t* mode = getPermisson(str);
+            doChmod(path, mode);
+            free(mode);
+            free(str);
+            isPlus = true;
+            isFirst = false;
+            j = 0;
+            continue;
+        }
         if (string[i] == '+') {
             isPlus = true;
             isFirst = false;
@@ -164,26 +201,13 @@ mode_t** getModes(char* string) {
     return modes;
 }
 
-void doChmod(char* fileName, mode_t* modes) {
-    int i = 1;
-    mode_t final_mode = modes[0];
-    while (modes[i] != 0) {
-        final_mode |= modes[i];
-        i++;
-    }
-    struct stat file_stat;
-    stat(fileName, &file_stat);
-    if (isPlus) chmod(fileName, file_stat.st_mode | final_mode);
-    else chmod(fileName, file_stat.st_mode & ~final_mode);
-}
-
 int main(int argc, char** argv) {
     if (argc != 3) {
         fprintf(stderr, "Incorrect arguments\n");
         return 1;
     }
     char* argument = argv[1];
-    char* path = argv[2];
+    path = argv[2];
 
     if (isdigit(argument[0])) {
         if (strlen(argument) != 3) {
