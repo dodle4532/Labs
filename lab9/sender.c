908dbee6f2411378bb261073ee82c4bec295bd4b
dodle4532 <120771908+dodle4532@users.noreply.github.com>
2024-12-04 14:45:55 +0300

    Create sender.c

---
+++ lab9/sender.c
@@ -0,0 +1,63 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
+
+char* addr;
+int shmid;
+int semid;
+
+void handle_sigterm(int sig) {
+    if (sig == SIGINT) {
+        printf("\n");
+    }
+    printf("End of sending\n");
+    shmdt(addr);
+    shmctl(shmid, IPC_RMID, NULL);
+    semctl(semid, 0, SETVAL, 1);
+    exit(0);
+}
+
+int main() {
+    key_t key = ftok("shm", 1);
+    shmid = shmget(key, 64, IPC_CREAT | 0666 | IPC_EXCL);
+    if (shmid < 0) {
+        int err = errno;
+        printf("shmget %s (%d)\n", strerror(err), err);
+        return 1;
+    }
+    semid = semget(key, 1, IPC_CREAT | 0666);
+    if (semid < 0) {
+        int err = errno;
+        printf("semget %s (%d)\n", strerror(err), err);
+        return 1;
+    }
+    addr = (char*)shmat(shmid, NULL, 0);
+    printf("Sending start, pid - %d\n", getpid());
+    if (addr == (void*)-1) {
+        int err = errno;
+        printf("shmat %s (%d)\n", strerror(err), err);
+        return 1;
+    }
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &handle_sigterm;
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+    while(1) {
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        sprintf(addr, "Pid of sender - %d, Time of sender - %02d:%02d:%02d", getpid(), tm.tm_hour, tm.tm_min, tm.tm_sec);
+        struct sembuf sop = {0, -1, 0};
+        semop(semid, &sop, 1);    
+        sleep(1);
+    }
+    return 0;
+}
