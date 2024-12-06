b9d2278d2dad5e842dff78d145ed99ce23b97bd6
dodle4532 <120771908+dodle4532@users.noreply.github.com>
2024-12-06 13:42:00 +0300

    Update mutex.c

--- lab8/mutex.c
+++ lab8/mutex.c
@@ -9,6 +9,7 @@ int curMax = 0;
 
 void* pthread_func_read(void* arg) {
     pthread_mutex_lock(&mtx);
+    usleep(100000);
     printf("My tid - %x\n", pthread_self());
     printf("Current max for mas - %d\n", curMax - 1);
     for (int i = 0; i < curMax; ++i) {
@@ -22,8 +23,10 @@ void* pthread_func_read(void* arg) {
 
 void* pthread_func_write(void* arg) {
     pthread_mutex_lock(&mtx);
+    usleep(100000);
     mas[curMax] = curMax;
     curMax++;
+    printf("Write ended\n");
     pthread_mutex_unlock(&mtx);
     pthread_exit(NULL);
 }
@@ -36,7 +39,7 @@ int main() {
     pthread_t threadWrite;
     for (int i = 0; i < 10; ++i) {
         pthread_create(&threadWrite, NULL, pthread_func_write, NULL);
-        usleep(200000);
+        //usleep(200000);
         pthread_create(&threadsRead[i], NULL, pthread_func_read, NULL);
     }
     for (int i = 0; i < 10; ++i) {
