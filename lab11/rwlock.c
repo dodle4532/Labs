e5a0f918ae8716ca2c2b2424026901ed9d1bc6c9
dodle4532 <120771908+dodle4532@users.noreply.github.com>
2024-12-13 19:10:21 +0300

    Update rwlock.c

--- lab11/rwlock.c
+++ lab11/rwlock.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <stdbool.h>
 
-pthread_mutex_t mtx;
-pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
-
+pthread_rwlock_t rwlock;
 
 int mas[10];
 int curMax = 0;
-bool isRead = true;
 
 void* pthread_func_read(void* arg) {
-    pthread_mutex_lock(&mtx);
-    printf("%d %d\n", curMax, *(int*)arg);
-    while (isRead) {
-        pthread_rwlock_wrlock(&rwlock);
+    int i = *(int*)arg;
+    pthread_rwlock_rdlock(&rwlock);
+    while (curMax <= i) { // Если еще не пора читать, то скинем мьютекс и попробуем еще раз
+        pthread_rwlock_unlock(&rwlock);
+        usleep(1000);
+        pthread_rwlock_rdlock(&rwlock);
+
     }
+    sleep(1);
     printf("My tid - %x\n", pthread_self());
     printf("Current max for mas - %d\n", curMax - 1);
     for (int i = 0; i < curMax; ++i) {
@@ -24,25 +24,18 @@ void* pthread_func_read(void* arg) {
     } 
     printf("\n");
     printf("\n");
-    isRead = true;
     pthread_rwlock_unlock(&rwlock);
-    pthread_mutex_unlock(&mtx);
     pthread_exit(NULL);
 }
 
 void* pthread_func_write(void* arg) {
-    pthread_mutex_lock(&mtx);
-    while (curMax < 10) {
-        int ret = 0;
-        while (isRead != true) {
-            pthread_rwlock_rdlock(&rwlock);
-        }
+    while(curMax < 10) {
+        pthread_rwlock_wrlock(&rwlock);
         mas[curMax] = curMax;
         curMax++;
         printf("Write ended\n");
-        isRead = false;
         pthread_rwlock_unlock(&rwlock);
-        pthread_mutex_unlock(&mtx);
+        sleep(1);
     }
     pthread_exit(NULL);
 }
@@ -50,17 +43,18 @@ void* pthread_func_write(void* arg) {
 
 
 int main() {
-    pthread_mutex_init(&mtx, NULL);
+    pthread_rwlock_init(&rwlock, NULL);
     pthread_t threadsRead[10];
     pthread_t threadWrite;
     pthread_create(&threadWrite, NULL, pthread_func_write, NULL);
     for (int i = 0; i < 10; ++i) {
         pthread_create(&threadsRead[i], NULL, pthread_func_read, (void*)&i);
-        usleep(50);
+        usleep(1); // гарантирует, что в arg все правильно передастся
     }
+    pthread_join(threadWrite, NULL);
     for (int i = 0; i < 10; ++i) {
         pthread_join(threadsRead[i], NULL);
     } 
-    pthread_mutex_destroy(&mtx);
+    pthread_rwlock_destroy(&rwlock);
     return 0;
 }
