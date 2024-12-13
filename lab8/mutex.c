18f18dc20a211b9cc258bfb960bb25db3b709e76
dodle4532 <120771908+dodle4532@users.noreply.github.com>
2024-12-13 19:15:32 +0300

    Update mutex.c

--- lab8/mutex.c
+++ lab8/mutex.c
@@ -2,19 +2,19 @@
 #include <unistd.h>
 #include <pthread.h>
 
-pthread_rwlock_t rwlock;
+pthread_mutex_t mtx;
 
 int mas[10];
 int curMax = 0;
 
 void* pthread_func_read(void* arg) {
     int i = *(int*)arg;
-    pthread_rwlock_rdlock(&rwlock);
+        //usleep(10000000);
+    pthread_mutex_lock(&mtx);
     while (curMax <= i) { // Если еще не пора читать, то скинем мьютекс и попробуем еще раз
-        pthread_rwlock_unlock(&rwlock);
+        pthread_mutex_unlock(&mtx);
         usleep(1000);
-        pthread_rwlock_rdlock(&rwlock);
-
+        pthread_mutex_lock(&mtx);
     }
     sleep(1);
     printf("My tid - %x\n", pthread_self());
@@ -24,37 +24,38 @@ void* pthread_func_read(void* arg) {
     } 
     printf("\n");
     printf("\n");
-    pthread_rwlock_unlock(&rwlock);
+    pthread_mutex_unlock(&mtx);
     pthread_exit(NULL);
 }
 
 void* pthread_func_write(void* arg) {
     while(curMax < 10) {
-        pthread_rwlock_wrlock(&rwlock);
+        pthread_mutex_lock(&mtx);
         mas[curMax] = curMax;
         curMax++;
         printf("Write ended\n");
-        pthread_rwlock_unlock(&rwlock);
+        pthread_mutex_unlock(&mtx);
         sleep(1);
     }
+
     pthread_exit(NULL);
 }
 
 
 
 int main() {
-    pthread_rwlock_init(&rwlock, NULL);
+    pthread_mutex_init(&mtx, NULL);
     pthread_t threadsRead[10];
     pthread_t threadWrite;
     pthread_create(&threadWrite, NULL, pthread_func_write, NULL);
     for (int i = 0; i < 10; ++i) {
         pthread_create(&threadsRead[i], NULL, pthread_func_read, (void*)&i);
-        usleep(1); // гарантирует, что в arg все правильно передастся
+        usleep(50); // гарантирует, что в arg все правильно передастся
     }
     pthread_join(threadWrite, NULL);
     for (int i = 0; i < 10; ++i) {
         pthread_join(threadsRead[i], NULL);
     } 
-    pthread_rwlock_destroy(&rwlock);
+    pthread_mutex_destroy(&mtx);
     return 0;
 }
