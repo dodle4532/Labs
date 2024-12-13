066d5856694bf3c412b0d17758e264365e9e5f45
dodle4532 <120771908+dodle4532@users.noreply.github.com>
2024-12-13 19:17:37 +0300

    Update cond.c

--- lab10/cond.c
+++ lab10/cond.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <stdbool.h>
 
 pthread_mutex_t mtx;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-
+pthread_cond_t cond;
 
 int mas[10];
 int curMax = 0;
-bool isRead = true;
 
 void* pthread_func_read(void* arg) {
+    int i = *(int*)arg;
     pthread_mutex_lock(&mtx);
-    while (isRead) {
-        pthread_cond_wait(&cond, & mtx);
+    while (curMax <= i) { // Ждем когда пора читать
+        pthread_cond_wait(&cond, &mtx);
     }
+    //printf("%d---%d\n", curMax, i);
     printf("My tid - %x\n", pthread_self());
     printf("Current max for mas - %d\n", curMax - 1);
     for (int i = 0; i < curMax; ++i) {
@@ -23,42 +22,40 @@ void* pthread_func_read(void* arg) {
     } 
     printf("\n");
     printf("\n");
-    isRead = true;
-    pthread_cond_signal(&cond);
+    sleep(1);
     pthread_mutex_unlock(&mtx);
     pthread_exit(NULL);
 }
 
 void* pthread_func_write(void* arg) {
-    while (curMax < 10) {
+    while(curMax < 10) {
         pthread_mutex_lock(&mtx);
-        while (!isRead) {
-            pthread_cond_wait(&cond, & mtx);
-        }
         mas[curMax] = curMax;
         curMax++;
         printf("Write ended\n");
-        isRead = false;
-        pthread_cond_signal(&cond);
+        pthread_cond_broadcast(&cond);
         pthread_mutex_unlock(&mtx);
+        sleep(1);
     }
+
     pthread_exit(NULL);
 }
 
-
-
 int main() {
     pthread_mutex_init(&mtx, NULL);
+    pthread_cond_init(&cond, NULL);
     pthread_t threadsRead[10];
     pthread_t threadWrite;
     pthread_create(&threadWrite, NULL, pthread_func_write, NULL);
     for (int i = 0; i < 10; ++i) {
         pthread_create(&threadsRead[i], NULL, pthread_func_read, (void*)&i);
-        usleep(1);
+        usleep(50); // гарантирует, что в arg все правильно передастся
     }
+    pthread_join(threadWrite, NULL);
     for (int i = 0; i < 10; ++i) {
         pthread_join(threadsRead[i], NULL);
     } 
     pthread_mutex_destroy(&mtx);
+    pthread_cond_destroy(&cond);
     return 0;
 }
