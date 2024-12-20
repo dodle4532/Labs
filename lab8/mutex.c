2ecf5a82713129abd4c083f20ec605374447d4f7
dodle4532 <120771908+dodle4532@users.noreply.github.com>
2024-12-20 13:20:53 +0300

    Update mutex.c

--- lab8/mutex.c
+++ lab8/mutex.c
@@ -9,22 +9,17 @@ int curMax = 0;
 
 void* pthread_func_read(void* arg) {
     int i = *(int*)arg;
-        //usleep(10000000);
-    pthread_mutex_lock(&mtx);
-    while (curMax <= i) { // Если еще не пора читать, то скинем мьютекс и попробуем еще раз
-        pthread_mutex_unlock(&mtx);
-        usleep(1000);
+    for (int i = 0; i < 10; ++i) {
         pthread_mutex_lock(&mtx);
+        printf("My tid - %x\n", pthread_self());
+        printf("Current max for mas - %d\n", curMax - 1);
+        for (int i = 0; i < curMax; ++i) {
+            printf("%d ", mas[i]);
+        } 
+        printf("\n\n");
+        pthread_mutex_unlock(&mtx);
+        sleep(1);
     }
-    sleep(1);
-    printf("My tid - %x\n", pthread_self());
-    printf("Current max for mas - %d\n", curMax - 1);
-    for (int i = 0; i < curMax; ++i) {
-        printf("%d ", mas[i]);
-    } 
-    printf("\n");
-    printf("\n");
-    pthread_mutex_unlock(&mtx);
     pthread_exit(NULL);
 }
 
@@ -37,11 +32,8 @@ void* pthread_func_write(void* arg) {
         pthread_mutex_unlock(&mtx);
         sleep(1);
     }
-
     pthread_exit(NULL);
 }
-
-
 
 int main() {
     pthread_mutex_init(&mtx, NULL);
@@ -52,6 +44,7 @@ int main() {
         pthread_create(&threadsRead[i], NULL, pthread_func_read, (void*)&i);
         usleep(50); // гарантирует, что в arg все правильно передастся
     }
+
     pthread_join(threadWrite, NULL);
     for (int i = 0; i < 10; ++i) {
         pthread_join(threadsRead[i], NULL);
